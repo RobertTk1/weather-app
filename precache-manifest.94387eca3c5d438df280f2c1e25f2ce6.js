299139582d18f28ff51f021be7c858ff3d724e90
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-01 05:53:24 +0000

    Deploy RobertTk1/weather-app to github.com/RobertTk1/weather-app.git:gh-pages

---
+++ precache-manifest.94387eca3c5d438df280f2c1e25f2ce6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fc3581ecec1ffbf89dd049c07cf7fcb",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "e297ad7f446cf63a5c17",
+    "url": "/weather-app/static/css/main.26621015.chunk.css"
+  },
+  {
+    "revision": "6268a75cb7938cd2944d",
+    "url": "/weather-app/static/js/2.36f1eb3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.36f1eb3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e297ad7f446cf63a5c17",
+    "url": "/weather-app/static/js/main.5cc47b2f.chunk.js"
+  },
+  {
+    "revision": "85de340fee6a1c87c645",
+    "url": "/weather-app/static/js/runtime-main.067d67ab.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/weather-app/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/weather-app/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
